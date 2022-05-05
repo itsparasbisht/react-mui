791e5c70fee5f85f87159dae26b3ea9e840278f9
itsparasbisht <iamparasbisht@gmail.com>
2022-05-05 10:52:11 +0530

    Mui menu

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MuiCard from "./components/MuiCard";
 import MuiAccordion from "./components/MuiAccordion";
 import MuiImageList from "./components/MuiImageList";
 import MuiNavbar from "./components/MuiNavbar";
+import MuiLink from "./components/MuiLink";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
       {/* <MuiAccordion /> */}
       {/* <MuiImageList /> */}
       <MuiNavbar />
+      <MuiLink />
     </div>
   );
 }
--- src/components/MuiNavbar.js
+++ src/components/MuiNavbar.js
@@ -1,15 +1,32 @@
-import { CatchingPokemon } from "@mui/icons-material";
+import {
+  CatchingPokemon,
+  CurrencyBitcoin,
+  KeyboardArrowDown,
+} from "@mui/icons-material";
 import {
   AppBar,
   Button,
   IconButton,
+  Menu,
+  MenuItem,
   Stack,
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 function MuiNavbar() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -29,8 +46,33 @@ function MuiNavbar() {
           <Button color="inherit">Features</Button>
           <Button color="inherit">Pricing</Button>
           <Button color="inherit">About</Button>
+          <Button
+            color="inherit"
+            id="resources-button"
+            onClick={handleClick}
+            endIcon={<KeyboardArrowDown />}
+          >
+            Resources
+          </Button>
           <Button color="inherit">Login</Button>
         </Stack>
+        <Menu
+          id="resources-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem>Blog</MenuItem>
+          <MenuItem>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
